812bb4603e668c17b103d81789df3326af0e04c8
sandernemvalts <sandernemvalts@ut.ee>
2026-03-03 13:40:22 +0200

    KdT4 Echo Server I

--- src/kdt/kdt4/Client.java
+++ src/kdt/kdt4/Client.java
@@ -1,9 +1,26 @@
 package kdt.kdt4;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
 public class Client {
 
-    static void main() {
+    static void main(String[] args) throws Exception {
 
+        System.out.println("Attempting connect");
+        try (Socket socket = new Socket("Localhost", 1337);
+             DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+             DataInputStream dis = new DataInputStream(socket.getInputStream())) {
+            System.out.println("\nConnection established");
+            dos.writeInt(args.length);
+            for (int i = 0; i < args.length; i++){
+                dos.writeUTF(args[i]);
+                String echo = dis.readUTF();
+                System.out.println("Server : " + echo);
+            }
+        }
+        System.out.println("\nprocess finished");
     }
 
 }
--- src/kdt/kdt4/Server.java
+++ src/kdt/kdt4/Server.java
@@ -1,9 +1,35 @@
 package kdt.kdt4;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
 public class Server {
 
-    static void main() {
+    static void main(String[] args) throws Exception{
 
+        try (ServerSocket ss = new ServerSocket(1337)){
+            System.out.println("live on socket 1337");
+            for (int i = 0; i < 10; i++) {
+
+                try (Socket socket = ss.accept();
+                     DataInputStream dis = new DataInputStream(socket.getInputStream());
+                     DataOutputStream dos = new DataOutputStream(socket.getOutputStream())
+                ){
+                    System.out.println("\nClient connected");
+                    int messageCount = dis.readInt();
+                    System.out.println("reading " + messageCount + " messages\n");
+                    for (int j = 0; j < messageCount; j++) {
+                        String message = dis.readUTF();
+                        System.out.print(message + " | ");
+                        dos.writeUTF(message);
+                    }
+                }
+                System.out.println("\nClient handled");
+            }
+        }
+        System.out.println("\nprocess finished");
     }
-
 }
